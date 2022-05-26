6734f991319064c50c85516e44ebeca0381c9ed7
seohyeon <tjgus9966@naver.com>
2022-05-26 20:36:02 +0900

    Update 42840 Solution

--- Programmers/Lv1. The Mock Test.js
+++ Programmers/Lv1. The Mock Test.js
@@ -24,3 +24,22 @@ function solution(answers) {
     }    
     return answer;
 }
+
+// 다른 사람 풀이를 참고한 풀이
+function solution(answers) {
+    var answer = [];
+    var a1 = [1, 2, 3, 4, 5];
+    var a2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+    var cnt1 = answers.filter((a, i) => a === a1[i % a1.length]).length;
+    var cnt2 = answers.filter((a, i) => a === a2[i % a2.length]).length;
+    var cnt3 = answers.filter((a, i) => a === a3[i % a3.length]).length;
+    var max = Math.max(cnt1, cnt2, cnt3);
+
+    if (cnt1 === max) {answer.push(1)};
+    if (cnt2 === max) {answer.push(2)};
+    if (cnt3 === max) {answer.push(3)};
+    
+    return answer;
+}
