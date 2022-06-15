44aa21b133195cca97dd9b43ca79e1a2bd9fbea0
seohyeon <tjgus9966@naver.com>
2022-06-15 21:17:40 +0900

    12945 Solution

---
+++ Programmers/Lv2. Fibonacci Number.js
@@ -0,0 +1,14 @@
+function solution(num) {
+    let answer = fibonacci(num);
+    return answer;
+}
+
+function fibonacci(n) {
+    let arr = [0, 1];
+    
+    for(let i = 2; i <= n; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+    }
+    
+    return arr[n];
+}
